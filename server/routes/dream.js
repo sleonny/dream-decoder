259dc63c56b05eb791865fe63073e67ef46ae1fa
Sean L <leonardmsean@gmail.com>
2025-05-30 13:11:11 -0400

    Test Route

---
+++ server/routes/dream.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/", (req, res) => {
+  const { dream } = req.body;
+  res.json({ message: "Interpreting dream: ${dream}" });
+});
+
+module.exports = router;
