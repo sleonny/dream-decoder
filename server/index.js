91b492d9e5114c0be6152530667bf745dd4c2186
Sean L <leonardmsean@gmail.com>
2025-05-30 13:07:33 -0400

    Starting to build routes

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+//Routes
+
+const dreamRoutes = require("./routes/dream");
+app.use("/api/dream", dreamRoutes);
+
+app.listen(PORT, () => {
+  console.log("Server running on port ${PORT}");
+});
